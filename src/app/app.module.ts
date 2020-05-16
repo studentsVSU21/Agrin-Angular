85a0d6f0971cd39497bb3ca2587279e9169e4ef7
Sigarev Aleksei <afakusima@mail.ru>
2020-05-16 11:15:32 +0300

    Added headet component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent} from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import {Routes, RouterModule} from '@angular/router';
+
+
+const appRoutes: Routes =[
+  { path: '', component: MainComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
