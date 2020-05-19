3a2594af19ccf5b3f9627c44641454162bc38fbe
Павел Парамонов <Pave_69@mail.ru>
2020-05-19 12:25:05 +0300

    Main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  imgLink = 'https://reedr.ru/wp-content/uploads/2019/04/DRON-1.jpg';
+  imgLingBackRef = 'https://wallbox.ru/resize/1920x1080/wallpapers/main/201126/e9131073ec61c9beaf13f555ee328f10.jpg';
   constructor() { }
 
   ngOnInit() {
